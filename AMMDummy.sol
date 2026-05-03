6a4f7307b49ef98cf9b70ebe76d1a013335272cc
nitsu123 <164813631+nitsu123@users.noreply.github.com>
2026-05-03 16:41:28 +0800

    Update AMMDummy.sol

--- AMMDummy.sol
+++ AMMDummy.sol
@@ -1,14 +1,19 @@
-/* SPDX-License-Identifier: GPL-3.0 */
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >0.8.0 <=0.9.0;
 
-contract AMM{
+contract AMM {
     address public owner;
     address public tokenA;
     address public tokenB;
     uint reserveA;
     uint reserveB;
     uint totalLiquidity;
+    
+    event LiquidityAdded(uint AmountA, uint AmountB, address LiquidityProvider);
+    event Swapped(uint AmountSwapped, address Swapper);
+    event LiquidityWithdrawn(uint AmountA, uint AmountB, address LiquidityProvider);
+
 
     mapping(address => uint) public userLP;
 
@@ -16,62 +21,175 @@ contract AMM{
         owner = msg.sender;
     }
 
-
-    function addLiuqidity(uint amountA, uint amountB) public {
-        require(amountA > 0 && amountB > 0, "invalid amount");
+    function addLiquidity(uint amountA, uint amountB) public payable {
         if(reserveA == 0 && reserveB == 0) {
             reserveA = amountA;
             reserveB = amountB;
 
-            uint liquidity = amountA + reserveA;
+            uint liquidity = amountA + amountB;
             totalLiquidity += liquidity;
-            userLP[msg.sender] += liquidity;
+            userLP[msg.sender]+= liquidity;
         }
         else {
-            require(amountA * reserveB == amountB * reserveA, "invalid ratio");
-            uint liquidity = (amountA * totalLiquidity) / reserveA;
-            totalLiquidity += liquidity;
-            userLP[msg.sender] += liquidity;  
+            require(reserveA * amountB == reserveB * amountA);
+                reserveA += amountA;
+                reserveB += amountB;
+
+                uint liquidity = (amountA * totalLiquidity) / reserveA;
+                totalLiquidity += liquidity;
+                userLP[msg.sender] += liquidity;
+            }
+        (bool success, ) = (tokenB).call(abi.encodeWithSignature("transfer(address, uint, uint)", msg.sender, amountA, amountB));
+        require(success, "add liq failed");
+        emit LiquidityAdded(amountA, amountB, msg.sender);
         }
-    }
+    
 
-    function swap(uint amountIn) public payable {
-        amountIn = msg.value;
-        uint confirmedIn = (amountIn * 990) / 1000;
-        require(confirmedIn > 0, "invalid amount");
+        function swap() public payable {
+            require(reserveA > 0 && reserveB > 0, "not enough liquidity");
+            uint countedIn = (msg.value * 999) / 1000;  
+            
+            uint oldA = reserveA;
+            uint oldB = reserveB;
 
-        uint oldA = reserveA;
-        uint oldB = reserveB;
 
-        uint newA = amountIn + oldA; 
-        uint newB = (oldA * oldB) / newA;
+            uint newA = countedIn + oldA;
+            uint newB = (oldA * oldB) / newA;
 
-        uint amountOut = newB - oldB;
+            reserveA = newA;
+            reserveB = newB;
 
-        (bool success, ) = tokenB.call(abi.encodeWithSignature("transfer(address, uint)", msg.sender, amountOut));
-        require(success, "transfer failed");
-    }
-    function withdrawLiquidity(uint _withdraw) public {
-        require(_withdraw <= userLP[msg.sender], "insufficient amount");
+            uint amountOut = oldB - newB;
+
+
+            (bool success, ) = (tokenB).call(abi.encodeWithSignature("transfer(address, uint)", msg.sender, amountOut));
+            require(success, "swap failed");
+
+
+            emit Swapped(amountOut, msg.sender);
+        }
+
+    function withdrawShare(uint _withdraw) public payable {
         require(_withdraw > 0, "invalid amount");
-        require(totalLiquidity > 0, "no liquidity to withdraw");
+        require(userLP[msg.sender] >= _withdraw, "you don not have any share");
+
+
         uint lpAmount = _withdraw;
 
-        uint ethOut = (lpAmount * reserveA) / totalLiquidity ;
-        uint tokenOut = (reserveB * lpAmount) /  totalLiquidity ;
+        userLP[msg.sender] -= lpAmount;
+        totalLiquidity -= lpAmount;
+
+        uint ethOut = ( lpAmount * reserveA) / totalLiquidity;
+        uint tokenOut = ( lpAmount * reserveB) /  totalLiquidity;
 
         reserveA -= ethOut;
         reserveB -= tokenOut;
-        
-        userLP[msg.sender] -= _withdraw;
-        totalLiquidity -= _withdraw;
 
-        (bool success, ) = (msg.sender).call{value: ethOut}("");
-        require(success, "withdraw failed");
-        (bool withdrawn, ) = (tokenB).call(abi.encodeWithSignature("transfer(address, uint)", msg.sender, tokenOut));
-        require(withdrawn, "USDC withdraw failed");
+        (bool success, ) = payable(msg.sender).call{value: ethOut}("");
+        require(success,"withdraw failed");
+
+        (bool withdrawn, ) = payable(tokenB).call(abi.encodeWithSignature("transfer(address, uint)", msg.sender, tokenOut));
+        require(withdrawn, "withdraw failed");
+
+        emit LiquidityWithdrawn(ethOut, tokenOut, msg.sender);
     }
 
-}
 
+}// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >0.8.0 <=0.9.0;
+
+contract AMM {
+    address public owner;
+    address public tokenA;
+    address public tokenB;
+    uint reserveA;
+    uint reserveB;
+    uint totalLiquidity;
+    
+    event LiquidityAdded(uint AmountA, uint AmountB, address LiquidityProvider);
+    event Swapped(uint AmountSwapped, address Swapper);
+    event LiquidityWithdrawn(uint AmountA, uint AmountB, address LiquidityProvider);
+
+
+    mapping(address => uint) public userLP;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addLiquidity(uint amountA, uint amountB) public payable {
+        if(reserveA == 0 && reserveB == 0) {
+            reserveA = amountA;
+            reserveB = amountB;
+
+            uint liquidity = amountA + amountB;
+            totalLiquidity += liquidity;
+            userLP[msg.sender]+= liquidity;
+        }
+        else {
+            require(reserveA * amountB == reserveB * amountA);
+                reserveA += amountA;
+                reserveB += amountB;
+
+                uint liquidity = (amountA * totalLiquidity) / reserveA;
+                totalLiquidity += liquidity;
+                userLP[msg.sender] += liquidity;
+            }
+        (bool success, ) = (tokenB).call(abi.encodeWithSignature("transfer(address, uint, uint)", msg.sender, amountA, amountB));
+        require(success, "add liq failed");
+        emit LiquidityAdded(amountA, amountB, msg.sender);
+        }
+    
+
+        function swap() public payable {
+            require(reserveA > 0 && reserveB > 0, "not enough liquidity");
+            uint countedIn = (msg.value * 999) / 1000;  
             
+            uint oldA = reserveA;
+            uint oldB = reserveB;
+
+
+            uint newA = countedIn + oldA;
+            uint newB = (oldA * oldB) / newA;
+
+            reserveA = newA;
+            reserveB = newB;
+
+            uint amountOut = oldB - newB;
+
+
+            (bool success, ) = (tokenB).call(abi.encodeWithSignature("transfer(address, uint)", msg.sender, amountOut));
+            require(success, "swap failed");
+
+
+            emit Swapped(amountOut, msg.sender);
+        }
+
+    function withdrawShare(uint _withdraw) public payable {
+        require(_withdraw > 0, "invalid amount");
+        require(userLP[msg.sender] >= _withdraw, "you don not have any share");
+
+
+        uint lpAmount = _withdraw;
+
+        userLP[msg.sender] -= lpAmount;
+        totalLiquidity -= lpAmount;
+
+        uint ethOut = ( lpAmount * reserveA) / totalLiquidity;
+        uint tokenOut = ( lpAmount * reserveB) /  totalLiquidity;
+
+        reserveA -= ethOut;
+        reserveB -= tokenOut;
+
+        (bool success, ) = payable(msg.sender).call{value: ethOut}("");
+        require(success,"withdraw failed");
+
+        (bool withdrawn, ) = payable(tokenB).call(abi.encodeWithSignature("transfer(address, uint)", msg.sender, tokenOut));
+        require(withdrawn, "withdraw failed");
+
+        emit LiquidityWithdrawn(ethOut, tokenOut, msg.sender);
+    }
+
+
+}
